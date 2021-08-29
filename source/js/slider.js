a240625746f5b3fe4c00ba3fe66f4cf7863b1cf0
cerasus1004 <cerasus1004@gmail.com>
2021-08-29 14:53:26 +0300

    Не работает слайдер

--- source/js/slider.js
+++ source/js/slider.js
@@ -4,84 +4,54 @@ import Swiper from 'swiper';
 
 
 (() => {
-  // new Swiper('.swiper-container', {
-  //   breakpoints: {
-  //     320: {
-  //       pagination: {
-  //         el: `.new-in__slider-pagination`,
-  //         type: `fraction`,
-  //         renderFraction: function (currentClass, totalClass) {
-  //           return `<span class=" ${currentClass} "></span> of <span class=" ${totalClass} "></span>`;
-  //         },
-  //       },
-  //       slidesPerView: 2,
-  //       spaceBetween: 30,
-  //       slidesPerGroup: 2
-  //     },
-  //     768: {
-  //       pagination: {
-  //         el: `.new-in__slider-pagination`,
-  //         type: `bullets`,
-  //         clickable: true,
-  //         renderBullet: function (index, className) {
-  //           return `<span class="${className}">${index + 1}</span>`;
-  //         },
-  //       },
-  //       slidesPerView: 2,
-  //       spaceBetween: 30,
-  //       slidesPerGroup: 2
-  //     },
-  //     1024: {
-  //       pagination: {
-  //         el: `.new-in__slider-pagination`,
-  //         type: `bullets`,
-  //         clickable: true,
-  //         renderBullet: function (index, className) {
-  //           return `<span class=" ${className} "> ${index + 1} </span>`;
-  //         },
-  //       },
-  //       slidesPerView: 4,
-  //       spaceBetween: 30,
-  //       //Кол-во пролистываемых слайдов
-  //       slidesPerGroup: 4
-  //     },
-  //   },
-
-  //   // Navigation arrows
-  //   navigation: {
-  //     nextEl: '.swiper-button-next',
-  //     prevEl: '.swiper-button-prev',
-  //   }
-  // });
-
-  const slider = document.querySelector(`.swiper-container`);
-
-if (slider) {
-  /* eslint-disable no-undef */
-  const mySwiper = new Swiper(`.swiper-container`, {
+  new Swiper('.new-in__slider', {
+    navigation: {
+      nextEl: '.new-in__slider-btn--next',
+      prevEl: '.new-in__slider-btn--prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: false,
+          type: 'fraction',
+          renderFraction: function (currentClass, totalClass) {
+            return '<span class="' + currentClass + '"></span>' + ' of ' + '<span class="' + totalClass + '"></span>';
+          },
+        },
+      },
+      768: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+          renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + '</span>';
+          },
+        },
+      },
+      1024: {
+        slidesPerView: 4,
+        slidesPerGroup: 4,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+          renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + '</span>';
+          },
+        },
+      }
+    },
     simulateTouch: false,
     spaceBetween: 30,
     watchOverflow: true,
     loop: true,
     speed: 800,
-    navigation: {
-      nextEl: `.slider-button-next`,
-      prevEl: `.slider-button-prev`,
-    },
-    slidesPerView: 4,
-    updateOnWindowResize: true,
-    breakpoints: {
-      320: {
-        width: 970
-      },
-      768: {
-        width: 1010
-      }
-    }
   });
-
-  mySwiper.init();
-  /* eslint-disable no-undef */
-}
-
 })();
