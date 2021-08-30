742bdc7722c4a7c4ed7f7cf9e7b937331671f0a0
cerasus1004 <cerasus1004@gmail.com>
2021-08-30 14:54:45 +0300

    3 блока main pp 3 экрана

--- build/js/main.js
+++ build/js/main.js
@@ -12410,23 +12410,26 @@ var __webpack_exports__ = {};
 
 (() => {
 const header = document.querySelector('.header');
+const headerContainer = header.querySelector('.header__container');
 const headerToggle = header.querySelector('.header__burger-btn');
 const listItems = header.querySelectorAll('.nav-list__link');
 const page = document.querySelector('.page');
 
 header.classList.remove('header--nojs');
+headerContainer.classList.remove('header__container--nojs');
 
-  const onMenuClick = function () {
-    console.log("1");
-    header.classList.toggle('header--js');
-    page.classList.toggle('page--js');
-  };
+const onMenuClick = function () {
+  console.log("1");
+  header.classList.toggle('header--js');
+  page.classList.toggle('page--js');
+  headerContainer.classList.toggle('header__container--js');
+};
 
-  for (let i = 0; i < listItems.length; i++) {
-    listItems[i].addEventListener('click', onMenuClick);
-  }
+for (let i = 0; i < listItems.length; i++) {
+  listItems[i].addEventListener('click', onMenuClick);
+}
 
-  headerToggle.addEventListener('click', onMenuClick);
+headerToggle.addEventListener('click', onMenuClick);
 
 })();
 
